5e56335ff8a342ef5bc6488b081889b58439650e
phankiet110901 <phankiet110901@gmail.com>
2020-05-11 13:36:04 +0700

    finish page detail movie

--- src/components/detailTabForDetailMoviePage.js
+++ src/components/detailTabForDetailMoviePage.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import './../assets/sass/main.scss';
 
 export default class DetailTabsForDetailMoivePage extends Component {
     render() {
         let { tabName, lichChieu } = this.props;
         let res = [];
         lichChieu.forEach((detailLichChieu, index) => {
-            if(detailLichChieu.thongTinRap.tenHeThongRap === tabName){
+            if (detailLichChieu.thongTinRap.tenHeThongRap === tabName) {
+                let thoiGianChieu = new Date(detailLichChieu.ngayChieuGioChieu);
                 res.push(
-                    <NavLink to="home/" key={index}>
-                        <p>{detailLichChieu.thongTinRap.tenRap}</p>
-                        <p>{detailLichChieu.tenPhim}</p>
-                    </NavLink>
+                    <div key={index} className="detailTab">
+                        <NavLink to="/home" >
+                            <p className="detail__name">{detailLichChieu.thongTinRap.tenRap}</p>
+                            <span className="detail__duration">{detailLichChieu.thoiLuong} phút - </span>
+                            <span className="detail__day">{`${thoiGianChieu.getDate()}.${thoiGianChieu.getMonth() + 1}.${thoiGianChieu.getFullYear()}`}</span>
+                            <p className="detail__price">Giá vé: {detailLichChieu.giaVe}</p>
+                            <p className="detail__time">Giờ chiếu: {`${thoiGianChieu.getHours()}:${thoiGianChieu.getUTCMinutes()}`}</p>
+                        </NavLink>
+                    </div>
                 );
             }
         });
